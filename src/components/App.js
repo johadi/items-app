a115df53e09012a0256cf90573c2849f3e5753c6
johadi <jimoh.hadi@gmail.com>
2018-10-29 21:10:39 +0100

    rename css app file

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import "../styles/App.css";
+import "../styles/app.css";
 import { items } from "../items/items";
 import Checkbox from "./Checkbox";
 import LinksView from "./LinksView";
@@ -19,7 +19,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <p className="help-text">
           Selecting an item will enable some boxes, based on condition, by
           vertically centering the corresponding box below
